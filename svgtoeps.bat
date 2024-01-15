94736aad177954526d6e2ddffcbea9b56b17ffc1
landcelita <landcelita@akane.waseda.jp>
2024-01-16 04:55:47 +0900

    先行研究1 done

--- svgtoeps.bat
+++ svgtoeps.bat
@@ -4,3 +4,9 @@ for /f "usebackq delims=" %%a in ( `dir *.svg /b /s` ) do (
         inkscape -p %%a -o %%a.eps
     )
 )
+
+for /f "usebackq delims=" %%a in ( `dir *.png /b /s` ) do (
+    if not exist %%a.eps (
+        inkscape -p %%a -o %%a.eps
+    )
+)
